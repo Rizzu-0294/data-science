f0437704d6635b90c33613f036eccb9e773522b6
Rizzu-0294 <141708959+Rizzu-0294@users.noreply.github.com>
2024-05-27 13:37:48 +0530

    Add files via upload

---
+++ assign.py
@@ -0,0 +1,22 @@
+#concatenation of two string 
+a="first"
+b=" second"
+print(a+b)
+
+# converting a string into lowercase 
+c="DID YOU SEE THAT?"
+print(c.lower())
+
+# string len
+d="hey how you doin"
+print(len(d))
+
+# removing whitespaces from the left of string 
+e="    hello buddy"
+print(e.lstrip())
+
+# difference between insert and append built in function 
+ls=["first","second","third","forth"]
+ls.append("last")
+ls.insert(4,"second last")
+print(ls)
